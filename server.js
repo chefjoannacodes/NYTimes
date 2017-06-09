b3c4d4cd81cb16afff3df258ea6591e015e1b103
Joanna Barajas <joannabarajas@joannabmacbook2.fios-router.home>
2017-06-08 17:50:58 -0700

    scrape from ny times website and get back raw object

---
+++ server.js
@@ -0,0 +1,90 @@
+/* Scraping into DB (18.2.5)
+ * ========================== */
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
+
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as a json
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  // Make a request for the news section of ycombinator
+  request("https://www.nytimes.com/pages/dining/index.html?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Food&WT.nav=page", function(error, response, html) {
+    // Load the html body from request into cheerio
+    var $ = cheerio.load(html);
+    // For each element with a "title" class
+    $(".story").each(function(i, element) {
+      // Save the text of each link enclosed in the current element
+      var title = $(this).children("h3").text();
+      // Save the href value of each link enclosed in the current element
+      var paragraph = $(this).children("p").text();
+
+      // If this title element had both a title and a link
+      if (title && paragraph) {
+        // Save the data in the scrapedData db
+        db.scrapedData.save({
+          title: title,
+          paragraph: paragraph
+        },
+        function(error, saved) {
+          // If there's an error during this query
+          if (error) {
+            // Log the error
+            console.log(error);
+          }
+          // Otherwise,
+          else {
+            // Log the saved data
+            console.log(saved);
+          }
+        });
+      }
+    });
+  });
+
+  // This will send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
